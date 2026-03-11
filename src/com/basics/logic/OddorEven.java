17a3cc4dcf39cb753283e9a7bf9c22ae843d4e93
suniljakkula <sunij8911@gmail.com>
2026-03-11 06:16:38 +0530

    basic even or odd program

--- src/com/basics/logic/OddorEven.java
+++ src/com/basics/logic/OddorEven.java
@@ -2,8 +2,22 @@ package com.basics.logic;
 
 public class OddorEven {
 
+	public static boolean isEven(int n) {
+		int rem = n % 2;
+		if (rem == 0) {
+			return true;
+		}
+		return false;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		int n = 14;
+		if (isEven(n) == true)
+			System.out.println("true");
+		else
+			System.out.println("false");
 
 	}
 
